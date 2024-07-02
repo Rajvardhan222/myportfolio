89574120d7148f4940600b416e5b52d30c671d02
Rajvardhan222 <146507318+Rajvardhan222@users.noreply.github.com>
2024-07-02 15:11:39 +0000

    final

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,9 +23,7 @@ const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
+const description = " I am a student who love to learn"
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
